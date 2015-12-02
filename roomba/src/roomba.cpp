f305c873ca1c38b8bf9332339fd5672b9a78acdf
Austin Ek <aek2@uic.edu>
2015-12-01 21:22:41 -0600

    Calculated coordinates for roomba

--- roomba/src/roomba.cpp
+++ roomba/src/roomba.cpp
@@ -1,87 +1,83 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <time.h>
 #include <math.h>
 
-void copterCallback(const std_msgs::String::ConstPtr& msg);                    //Prints the data returned from the subscriber
+geometry_msgs::PoseStamped poseMsg;
 
-void copterCallback(const std_msgs::String::ConstPtr& msg)
+void copterCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+	poseMsg = *msg;
+	ROS_INFO_STREAM("I heard: " << poseMsg.pose.position.x);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "roomba");
+ 	ros::init(argc, argv, "roomba");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
+ 	ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Publisher pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);         //This is used to publish messages
+ 	ros::Publisher pubMov = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);		//This is used to publish movement messages
 
-  ros::Subscriber sub = n.subscribe("copter", 1000, copterCallback);            //Subscribe to the copters messages
-  //copter is the name of the node being subscribed to
+ 	ros::Subscriber sub = n.subscribe("copter", 1, copterCallback);					//Subscribe to the copters messages
 
-  ros::Rate loop_rate(10);                                                      //This determines the length of time between loop iterations
-  geometry_msgs::Twist msg;                                                     //Twist object to publish messages
+ 	ros::Rate loop_rate(10);														//This determines the length of time between loop iterations
+ 	geometry_msgs::Twist mov;														//Twist object to publish messages
 
-  bool forward = true;                                                          //Boolean to determine the direction and switch faster
-  int last_20;                                                                  //The last number on the 20 second interval
-  int last_5;                                                                   //The last number on the 5 second interval
-  int sim_time;                                                                 //The time of the current simulation in seconds
-  double dir;                                                                   //The direction to change on the 5 second intervals
-  //simTime is an int because anything less than a second is never used
+ 	bool forward = true;															//Boolean to determine the direction and switch faster
 
-  /* This program changes the direction of the roomba every 20 seconds.
-   * 
-   */
+ 	int last_20;																	//The last number on the 20 second interval
+ 	int last_5;																		//The last number on the 5 second interval
+ 	int last_1;																		//The last number of the 1 second interval
+ 	int sim_time;																	//The time of the current simulation in seconds
 
-  while (ros::ok())
-  {
-    if (forward) msg.linear.x = 0.33;                                           //The speed is 0.33m/s
-    else msg.linear.x = -0.33;
+ 	double dir;																		//The direction to change on the 5 second intervals
+ 	double total_dist = 0;															//The total distance traveled so far
+ 	double total_ang = 0;															//The total angle turned so far
 
-    sim_time = ros::Time::now().toSec();                                        //Set sim_time to the current time
+ 	double x_dist = 0;																//The distance traveled in the x direction
+ 	double z_dist = 0;																//The distance traveled in the z direction
 
-    ROS_INFO_STREAM("Time is " << sim_time);                                    //Print the time to the console
+ 	while (ros::ok())
+ 	{
+   		if (forward) mov.linear.x = 0.33;											//The speed is 0.33m/s
+    	else mov.linear.x = -0.33;
 
-    if (sim_time % 20 == 0 && sim_time != last_20){                             //Switch the direction every 20 seconds
-      forward = !forward;
-      last_20 = sim_time;
-    }
+		sim_time = ros::Time::now().toSec();										//Set sim_time to the current time
 
-    if (sim_time % 5 == 0 && sim_time != last_5){                               //Change direction every 5 seconds
-      dir = (rand() % 41) - 20;    	                                            //Random number 0-pi/9
-      msg.angular.z = dir;                                                      //Change the direction based on that number
-      last_5 = sim_time;
-    }
-    else msg.angular.z = 0;                                                     //Otherwise, don't change direction
+		if (last_1 != sim_time){													//If a second has passed
+			total_dist += mov.linear.x;												//Add the amount traveled in that second
+			last_1 = sim_time;														//Reassign last_1 so you don't add it every iteration
+			z_dist += (mov.linear.x) * sin(total_ang);
+			x_dist += (mov.linear.x) * cos(total_ang);
+		}
+
+		ROS_INFO_STREAM("Time is " << sim_time);									//Print the time to the console
+		//ROS_INFO_STREAM("Distance traveled is " << total_dist);					//Print the total distance traveled to the console
+		//ROS_INFO_STREAM("Angle turned is " << total_ang);							//Print the total angle turned
+		ROS_INFO_STREAM("X distance is " << x_dist);
+		ROS_INFO_STREAM("Z distance is " << z_dist);
+
+		if (sim_time % 20 == 0 && sim_time != last_20){								//Switch the direction every 20 seconds
+			forward = !forward;
+			last_20 = sim_time;
+		}
+
+		if (sim_time % 5 == 0 && sim_time != last_5){								//Change direction every 5 seconds
+			dir = (rand() % 41) - 20;												//Random number 0-20 (degrees in either direction)
+			//dir = (3.14159/180) * dir;												//Convert the degrees to radians
+			mov.angular.z = dir;													//Change the direction based on that number
+			last_5 = sim_time;
+			total_ang += dir;														//Add the angle turned to total_ang
+		}
+		else mov.angular.z = 0;														//Otherwise, don't change direction
  
-    pub.publish(msg);                                                           //Publish the message
-    ros::spinOnce();
-    loop_rate.sleep();                                                          //Wait until the next iteration
-  }
+		pubMov.publish(mov);														//Publish the movement message
 
-  return 0;
+		ros::spinOnce();
+		loop_rate.sleep();															//Wait until the next iteration
+	}
+
+	return 0;
 }
