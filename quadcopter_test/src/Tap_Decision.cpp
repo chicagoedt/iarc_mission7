4f8d85e35be6d08d3dad9671e0488a7ff0e8b494
Logesh Roshan <lramad2@uic.edu>
2016-01-29 21:24:42 -0600

    Roomba now visible in Rviz. TF tree now connects world and roomba. Decision making on tapping roomba.

---
+++ quadcopter_test/src/Tap_Decision.cpp
@@ -0,0 +1,105 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include "Tap_Decision.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <math.h>
+#include <std_msgs/Bool.h>
+
+Decision::Decision():
+	nh(),
+	loop_rate(1)
+{
+	checker = 1; // checker to check rate of change of roomba. rotates b/w the if-else in callback.
+	initdec = 1; // initially declare feedbackMsgrp2 as 0. This should happen only once. hence global.
+	timer = 1;
+	publ = nh.advertise<std_msgs::Bool>("Tap", 1); // FIXIT (std_msgs::Int16)publishing the values to move the qc, currently publishes a velociy in a direction 	
+	subrp = nh.subscribe("roomba/pose", 1, &Decision::callbackrp, this); // subscribe to roomba pose, revise after Harsh chooses ros topic
+	ROS_INFO_STREAM("Initialized Topics!");
+}
+
+Decision::~Decision()
+{
+
+}
+
+void Decision::callbackrp(const geometry_msgs::PoseStamped::ConstPtr& posrp)
+{
+	if(initdec == 1)
+	{	
+		feedbackMsgrp2.pose.position.x = 0;
+		feedbackMsgrp2.pose.position.y = 0;
+		feedbackMsgrp2.pose.position.z = 0;
+		initdec = 0;
+		
+	}	
+// Calculate RateOfChange in x and y component.
+	if(checker==1) // constantly alters new and old position.once 1:new & 2:old. Next 1:old & 2:new.
+	{
+		feedbackMsgrp1 = *posrp;
+		dxbydt = (feedbackMsgrp1.pose.position.x - feedbackMsgrp2.pose.position.x);
+		dybydt = (feedbackMsgrp1.pose.position.y - feedbackMsgrp2.pose.position.y);
+		dxbydt = dxbydt/0.2; // dividing to get rate of change. 0.2 b/c the freq is 5. Hence time diff b/w two pos. is 0.2s.
+		dybydt = dybydt/0.2;
+		checker = 0;
+		
+	}
+	else
+	{
+		feedbackMsgrp2 = *posrp;
+		dxbydt = feedbackMsgrp2.pose.position.x - feedbackMsgrp1.pose.position.x;
+		dybydt = feedbackMsgrp2.pose.position.y - feedbackMsgrp1.pose.position.y;
+		dxbydt = dxbydt/0.2; // dividing to get rate of change. 0.2 b/c the freq is 5. Hence time diff b/w two pos. is 0.2s.
+		dybydt = dybydt/0.2;
+		checker = 1;
+		
+	}
+		
+}
+
+void Decision::Calculate()
+{
+	std_msgs::Bool canTap;
+	canTap.data = false; // 0 = false, 1 = true.
+	//int SecBwTap = 4; // Time between allowing taps
+	ros::Duration seconds(4);
+	if(timer == 1)
+	{
+		begin = ros::Time::now();
+		timer = 0;
+	}
+	/*if(ros::Time::now()-begin<seconds)//ros::Duration(SecBwTap)) // FIXME!!!!!!!!!!!!!!!
+	{
+		canTap.data = false;
+	}
+	else
+	{*/
+		if(dybydt<0)
+		{
+			canTap.data = true;
+			std::cout <<"RAAAAAAAAAAAAAAAN";
+		}
+		else if((atan(dybydt/dxbydt)>=-0.785) && (atan(dybydt/dxbydt)<=0.524))
+		{
+			canTap.data = true;
+		}
+		else
+		{
+			canTap.data = false;
+		}
+		timer = 1;
+	//}
+	std::cout << "\ndybydt in Decision" << dybydt << "\n"; 
+	std::cout <<"\ncanTap in Decision:" << canTap << "\n";
+	publ.publish(canTap);
+}
+
+void Decision::run()
+{	
+	while(ros::ok())
+	{
+		Calculate();
+		ros::spinOnce();
+    	loop_rate.sleep();
+	}
+
+}
--- quadcopter_test/src/vector_calc.h
+++ quadcopter_test/src/vector_calc.h
@@ -2,7 +2,7 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Path.h>
-
+#include <std_msgs/Bool.h>
 
 class vector
 {
@@ -22,14 +22,20 @@ private:
 	ros::Rate loop_rate;
 	ros::Subscriber subqcp;
 	ros::Subscriber subrp;  // subqcp subscribes to groundtruth of quadcopter. subrp to twist of roomba
+	ros::Subscriber subtap;
 	int state;
 	int timecheck;
-	float dxbydt;
-	float dybydt;
+	float dxbydt; // rate of change along x axis
+	float dybydt; // rate of change along y axis
+/*x2, y2 are global since they shouldn't change when descending.
+z2 isn't global since z-component of roomba is always zero
+x2, y2, z2 are the estimated points that the QC must descend to.
+*/
 	float x2;
 	float y2;
 	int checker; // checker to check rate of change of roomba. rotates b/w the if-else in callback.
 	int initdec; // initially declare feedbackMsgrp2 as 0. This should happen only once. hence global.
+	
 	ros::Time begin;
 	//ros::Duration zero(0.0);
 	
@@ -37,9 +43,35 @@ private:
 	geometry_msgs::PoseStamped feedbackMsgrp; // to store position of roomba
 	geometry_msgs::PoseStamped feedbackMsgrp1;// To find rate of change of roomba. old position
 	geometry_msgs::PoseStamped feedbackMsgrp2;// ^ new position.
+	std_msgs::Bool Tap_it;
+
+	int i;
+	int timerun;
+	float x1;
+	float y1;
+	float z1;
+	float z2; // height it'd reach when going down.
+	float vmag;
+	float vmag2;
+	float constvelsq; // constant velocity squared for the velocity you want it to go in
+	float unitx1;
+	float unity1;
+	float unitz1;
+	float unitx2;
+	float unity2;
+	float unitz2;
+	float c; // constant for calculation
+	float disp; // vector variables to point direction
+	int npts; //no. of pts for rviz path
+	float dpts;  // distance b/w pts
+	float rx;
+	float ry;
+	float rz; //  rviz coordinates
 
  	void calculate();
+	void Init();
  	void callbackqcp(const geometry_msgs::PoseStamped::ConstPtr& posqcp); // sub callback for qcp
 	void callbackrp(const geometry_msgs::PoseStamped::ConstPtr& posrp); // sub callback for rp
+	void callbacktap(const std_msgs::Bool::ConstPtr& Tap_Sub); // Tap Subscriber
 
 };
